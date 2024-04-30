fc25b2ffe0b3f06f96568e45d8ec839f3131db1c
Miles Granger <miles59923@gmail.com>
2024-04-30 17:43:48 +0200

    Make own cpu supports?

--- blosc2-sys/src/lib.rs
+++ blosc2-sys/src/lib.rs
@@ -7,12 +7,25 @@
 )]
 
 pub use libc;
-use libc::{timespec, FILE};
+use libc::{c_char, timespec, FILE};
 
 #[cfg(not(feature = "regenerate-bindings"))]
 mod bindings;
 #[cfg(not(feature = "regenerate-bindings"))]
 pub use bindings::*;
+
+extern "C" {
+    fn ruapu_init();
+    fn ruapu_supports(isa: *const c_char) -> i32;
+}
+
+#[no_mangle]
+pub extern "C" fn __builtin_cpu_supports(n: *const c_char) -> bool {
+    unsafe {
+        ruapu_init();
+        ruapu_supports(n) != 0
+    }
+}
 
 #[cfg(feature = "regenerate-bindings")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
